3836e645a56c122764a969a0354841ea3f414988
WiseDodge <wisedodgecoding@gmail.com>
2025-08-19 10:56:47 -0400

    feat(group_locator): added get_group_roles and get_group_join_requests

--- src/roblox_opencloud/group_locator.py
+++ src/roblox_opencloud/group_locator.py
@@ -16,3 +16,13 @@ class GroupLocatorClient:
         if page_token:
             endpoint += f"&pageToken={page_token}"
         return self.api_client.get(endpoint)
+
+    def get_group_roles(self, group_id):
+        endpoint = f"{self.endpoint}{group_id}/roles"
+        return self.api_client.get(endpoint)
+
+    def get_group_join_requests(self, group_id, max_page_size=20, page_token=None):
+        endpoint = f"{self.endpoint}{group_id}/join-requests?maxPageSize={max_page_size}"
+        if page_token:
+            endpoint += f"&pageToken={page_token}"
+        return self.api_client.get(endpoint)
