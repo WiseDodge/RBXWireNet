a5b5a4c515d7a836c1496211f00d6706e831a949
WiseDodge <wisedodgecoding@gmail.com>
2025-08-19 09:31:41 -0400

    fix(config): updated BASE_URL to Roblox Cloud v2 API base URL

--- src/roblox_opencloud/config.py
+++ src/roblox_opencloud/config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()  # Loads .env if present
 
 RBXWIRENET_GROUP_API_KEY = os.getenv("RBXWIRENET_GROUP_API_KEY")
-BASE_URL = "https://apis.roblox.com"
+BASE_URL = "https://apis.roblox.com/cloud/v2/"
 
 if not RBXWIRENET_GROUP_API_KEY:
     raise ValueError("RBXWIRENET_GROUP_API_KEY is not set in environment variables.")
